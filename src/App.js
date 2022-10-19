9097badd368da50cdf6e19429a47e5035d55b483
kyrietheblackitachi <robertkyrie100@gmail.com>
2022-10-19 01:09:09 +0100

    deleted unnecssary json

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Home from './Home.js'
 import { Box } from '@mui/system'
 
-import Display from './Display.js'
-
 function App() {
   return (
       <Box><Home/></Box>
